ef67eb196df443eded49acc8eb2ef0462b5b5c07
olaAlfredo <128110087+olaAlfredo@users.noreply.github.com>
2024-08-28 21:24:01 -0700

    Add files via upload

---
+++ Ejercicio4.js
@@ -0,0 +1,5 @@
+// 4. Ordenamiento de un arreglo: 
+// Crea un arreglo de números desordenados y luego ordénalo de menor a mayor utilizando algún algoritmo de ordenamiento (p. ej., el método sort()).
+const arreglo1 = [4,2,8,9,0,11,3];
+arreglo1.sort((a, b) => a-b);
+console.log("4. El arreglo ordenado es: "+arreglo1+"");
